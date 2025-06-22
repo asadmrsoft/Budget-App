3c712ff388e2ab2a8c8baaae77a5ae60ee889765
asadmrsoft <asadmrsoft@gmail.com>
2025-06-22 08:27:16 +0530

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.find(1)
+cat = Category.find_by(name: 'Groceries')
+Transaction.create!(amount: 199.99, description: 'Supermarket run', user: user, category: cat)
+Transaction.create!(amount: 49.50, description: 'Dinner order', user: user, category: cat)
